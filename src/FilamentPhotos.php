30497402ae99ceb5728eceda5448350e7015498b
aspirantzhang <admin@aspirantzhang.com>
2025-07-13 13:22:55 +0800

    feat: implement categoryPhotos helper function to retrieve photos by category

--- src/FilamentPhotos.php
+++ src/FilamentPhotos.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace RectitudeOpen\FilamentPhotos;
 
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
 use RectitudeOpen\FilamentPhotos\Models\Photo;
 
 class FilamentPhotos
@@ -24,5 +25,18 @@ class FilamentPhotos
     {
         // @phpstan-ignore-next-line
         return $this->getModel()::with(['categories'])->ordered()->published()->paginate($perPage);
+    }
+
+    /**
+     * @return Collection<int, Photo>
+     */
+    public function getPublishedPhotosByCategory(int $categoryId): Collection
+    {
+        $carousels = $this->getModel()::published()->whereHas('categories', function ($query) use ($categoryId) {
+            $query->where('id', $categoryId);
+        })->get();
+
+        /** @var Collection<int, Photo> $carousels */
+        return $carousels;
     }
 }
---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Eloquent\Collection;
+use RectitudeOpen\FilamentPhotos\FilamentPhotos;
+use RectitudeOpen\FilamentPhotos\Models\Photo;
+
+if (! function_exists('photosByCategory')) {
+    /**
+     * @return Collection<int, Photo>
+     */
+    function categoryPhotos(int | string $categoryIdentifier): Collection
+    {
+        $service = app(FilamentPhotos::class);
+
+        if (is_numeric($categoryIdentifier)) {
+            return $service->getPublishedPhotosByCategory((int) $categoryIdentifier);
+        }
+
+        return new Collection;
+    }
+}
